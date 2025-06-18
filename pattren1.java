c67266448ba4d5e12a78497c22f90737862b7ad9
naveendusanapudi <naveendusanapudi.123@gmail.com>
2025-06-18 13:03:17 +0530

    Add files via upload

---
+++ pattren1.java
@@ -0,0 +1,223 @@
+// 1.
+// import java.util.*;
+// class pattren1
+// {
+	// public static void main(String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int n=p.nextInt();
+		// for (int i=0;i<n;i++)
+		// {
+			// for(int j=0;j<n;j++ )
+			// {
+			// System.out.print("*");
+			// }
+			// System.out.println();			
+			
+		// }
+	// }
+// }
+
+// 2.
+// import java.util.*;
+// class pattren1
+// {
+	// public static void main(String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int n=p.nextInt();
+		// for (int i=1;i<=n;i++)
+		// {
+			// for ( int j=0;j<n;j++){
+			// System.out.print(i);
+			// }
+			// System.out.println();
+		// }
+	// }
+// }
+
+// 3.
+// import java.util.*;
+// class pattren1
+// {
+	// public static void main(String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int n=p.nextInt();
+		// for (int i=0;i<=n;i++)
+		// {
+			// for (int j=1;j<=n;j++)
+			// {
+			// System.out.print(j);
+			// }
+			// System.out.println();
+		// }
+	// }
+// }
+
+// 4.
+// import java.util.*;
+// class pattren1
+// {
+	// public static void main(String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int k=1;
+		// int n=p.nextInt();
+		// for (int i=1;i<=n;i++)
+		// {
+			// for(int j=0;j<n;j++)
+			// {
+				// System.out.print(k);
+				// System.out.print("");
+				// k++;
+			// }
+			// System.out.println();
+		// }
+	// }
+// }
+
+// 5.
+// import java.util.*;
+// class pattren1
+// {
+	// public static void main(String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int n=p.nextInt();
+		// int k=1;
+		// for (int i=1;i<=n;i++)
+		// {
+			// for(int j=0;j<n;j++)
+			// {
+				// System.out.print(k+" ");
+				// if(k==9)
+				// {
+					// k=1;
+					// continue;
+				// }
+				// k++;
+			// }
+			// System.out.println();
+		// }
+	// }
+// }
+
+// 6.
+// import java.util.*;
+// class pattren1
+// {
+	// public static void main (String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int n=p.nextInt();
+		// int k=1;
+		// for (int i=0;i<n;i++)
+		// {
+			// for (int j=0;j<n;j++)
+			// {
+				// System.out.print(k+" ");
+				// k=k+2;
+			// }
+			// System.out.println();
+		// }
+	// }
+// }
+
+// 7.
+// import java.util.*;
+// class pattren1
+// {
+	 // public static void main (String [] args)
+	 // {
+		 // int n=5;
+		// for(int i=1;i<=n;i++)
+		// {
+			// for(int j=1;j<=n;j++)
+			// {
+				// if((j%2)==0)
+				// {
+					// System.out.print("0");
+				// }
+				// else
+				// {
+					// System.out.print("1");
+				// }
+			// }
+			// System.out.println();
+		// }
+	 // }
+	// }
+	// 8
+	// import java.util.*;
+	// class pattren1
+	// {
+	
+	// public static void main (String [] args)
+	// {
+		// Scanner p = new Scanner(System.in);
+		// int n=p.nextInt();
+		// for(int i=1;i<=n;i++)
+		// {
+			// for(int j=1;j<=(n-i);j++)
+			// {
+				// System.out.print(" ");
+			// }
+			// for(int j=1;j<=i;j++)
+			// {
+				// System.out.print(" *");
+			// }
+			// System.out.println();
+		// }
+	// }
+	// }
+	
+	// 9
+	// import java.util.*;
+	// class pattren1
+	// {
+	// int a=0;
+		// for(int i=1;i<=n;i++)
+		// {
+			// for(int j=1;j<=(n-i);j++)
+			// {
+				// System.out.print(" ");
+			// }
+			
+			// for(int j=1;j<=(i*2-1);j++)
+			// {
+				// j++;
+				// System.out.print(" "+j);
+				
+			// }
+			// System.out.println();
+		// }
+	// }
+	
+// 10.
+	// class pattren1
+	// {
+		 // public static void main (String [] args)
+		 // {
+			// String n="INDIA";
+			// for (int i=0;i<6;i++){
+				// for (int j=0;j<i;j++){
+					// System.out.print(n.charAt(j));
+					// }
+				// System.out.println();
+			// }
+			
+			// int n=5;
+			// for(int i=1;i<n+1;i++){
+				// for (int j=1;j<n+1-i;j++){
+						// System.out.print(" ");
+					// }
+				// for(int k=1;k<=(2*i-1);k++){
+						// System.out.print("*");
+					// }
+				
+				// System.out.println();
+						
+			// }	
+		 // }
+	// }
